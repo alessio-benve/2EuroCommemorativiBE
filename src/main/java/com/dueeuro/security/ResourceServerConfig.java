420f373a92feb8ed96480f9fe3d4799bb293300f
Alessio <alessio.benve.15@gmail.com>
2020-01-30 23:46:59 +0100

    modificaFull

--- src/main/java/com/dueeuro/security/ResourceServerConfig.java
+++ src/main/java/com/dueeuro/security/ResourceServerConfig.java
@@ -31,7 +31,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Bean
 	public CorsConfigurationSource cosrsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("https://2eurocommemorativi.netlify.com")); // indirizzo del client
+		config.setAllowedOrigins(Arrays.asList("*")); // indirizzo del client
 		config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type","Authorization"));		
